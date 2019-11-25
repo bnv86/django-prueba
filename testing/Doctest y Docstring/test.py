105b60235a291e2e0d5d3fc61b2012a6ab3d55c3
bruno vitucci <vituccibruno@gmail.com>
2019-11-24 23:37:37 -0300

    paso de pytest xq hay conflictos con python3.7.4, igual la que importa es unittest2, sigo con ella

---
+++ testing/Doctest y Docstring/test.py
@@ -0,0 +1,6 @@
+from algoritmos import fibonacci, palindromo, factorial
+import pytest
+#pytest
+
+def test_fibonacci_cinco():
+    assert fibonacci(5) == 5
---
+++ testing/shopping_cart/test_shopping_cart.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestShoppingCart(unittest.TestCase):
+
+    def test_cinco_mas_cinco_igual_diez(self):
+        assert 5 + 5 == 10
+
+
+if __name__ == '__main__':
+    unittest.main()
