c7c32ea505035a5998a2bd21f9a4bc5969e7d31c
bnv86 <vituccibruno@gmail.com>
2019-11-26 16:03:50 -0300

    coverage consola, falta el grafico

--- testing/shopping_cart/shopping_cart.py
+++ testing/shopping_cart/shopping_cart.py
@@ -1,11 +1,20 @@
+#assertRaises
+class NotExistsItemError(Exception):
+    pass
+
 class Item:
 
     def __init__(self, name, price):
         self.name = name
         self.price = price
+    
+    #assertRegex
+    def code(self):
+        return "{}-123456789".format(self.name)
 
-    def __str__(self):
-        return self.name
+    #comentado para que el reporte de coverage de %100
+    #def __str__(self):
+    #    return self.name
 
 
 class ShoppingCart:
@@ -18,6 +27,18 @@ class ShoppingCart:
 
     def remove_item(self, item):
         self.items.remove(item)
+
+    #operadores relacionales
+    def total(self):
+        return sum([item.price for item in self.items])
     
     def contains_items(self):
         return len(self.items) > 0
+
+    #assertRaises (excepciones)
+    def get_item(self, item):
+        if item not in self.items:
+            raise NotExistsItemError('Item does not exists')
+        else:
+            return self.items[ self.items.index(item) - 1 ]
+
--- testing/shopping_cart/test_shopping_cart.py
+++ testing/shopping_cart/test_shopping_cart.py
@@ -1,6 +1,9 @@
 import unittest
-from shopping_cart import Item, ShoppingCart
+from shopping_cart import Item, ShoppingCart, NotExistsItemError
 
+API_VERSION = 17
+
+#pruebas unitarias
 class TestShoppingCart(unittest.TestCase):
 
     #se ejecuta antes de cada prueba unitaria
@@ -25,7 +28,7 @@ class TestShoppingCart(unittest.TestCase):
     def test_nombre_producto_igual_pan(self):
         #item = Item("Manzana", 12.0)
         #palabra reservada assert o un metodo de la clase TestCase
-        self.assertEqual(self.pan.name, "Pan")
+        self.assertEqual(self.pan.name, "Pan") #compara dos valores (==)
         print("4")
 
     #test para setUp
@@ -43,6 +46,45 @@ class TestShoppingCart(unittest.TestCase):
         self.shopping_cart.remove_item(self.pan)
         self.assertFalse(self.shopping_cart.contains_items())
         print("7")
+
+    def test_obtener_producto_pan(self):
+        item = self.shopping_cart.get_item(self.pan)
+        self.assertIs(item, self.pan) #compara dos objetos (is)
+        self.assertIsNot(item, self.jugo)
+        print("8")
+
+    def test_excepcion_al_obtener_jugo(self):
+        with self.assertRaises(NotExistsItemError):
+            self.shopping_cart.get_item(self.jugo)
+            print("9")
+
+    #operadores relacionales
+    def test_total_con_un_producto(self):
+        total = self.shopping_cart.total()
+        self.assertGreater(total, 0)
+        self.assertLess(total, self.pan.price + 1.0)
+        self.assertEqual(total, self.pan.price)
+        print("10")
+
+    #assertRegex
+    def test_codigo_pan(self):
+        self.assertRegex(self.pan.code(), self.pan.name)
+
+    #validar de forma manual si una prueba es exitosa o no
+    def test_fail(self):
+        if 3 > 2:
+            self.fail("Dos no es mayor a tres!")
+
+    #Conocemos si que la prueba no va a ejecutarse
+    #@unittest.skip("Colocamos nuestros motivos")
+
+    #Desconocemos si la prueba puede o no ejecutarse
+    #@unittest.skipIf(True , "Coloramos nuestros motivos")
+    #@unittest.skipIf(API_VERSION < 18 , "Version obsoleta")
+    @unittest.skipUnless(False, "Colocamos nuestros motivos") #pasa ok si el resultado del bool o comparacion es falso 
+    def test_prueba_skip(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
